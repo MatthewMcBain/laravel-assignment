e8caf5dd7dfe31bdc287199ddd1b20c5280ef390
Matthew McBain <matthewmcbain3@gmail.com>
2024-05-01 12:58:45 +0100

    change back to pizza list

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -1,24 +1,22 @@
 <x-app-layout>
-    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+   <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Pizzas') }}
         </h2>
 
-        <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+       <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
             @foreach ($pizzas as $pizza)
-            <div class="p-6 flex space-x-2">
-                <div class="flex-1">
-                    <div class="flex justify-between items-center">
-                        <div>
-                        <fieldset>
-                            <input type="checkbox" name="pizzas[]" value="{{ $pizza->pizza_name }}">{{ $pizza->pizza_name }}
-                        </fieldset>
+                <div class="p-6 flex space-x-2">
+                    <div class="flex-1">
+                        <div class="flex justify-between items-center">
+                            <div>
+                                <span class="text-gray-800">{{ $pizza->pizza_name }}</span>
+                            </div>
                         </div>
+                        <p class="mt-4 text-lg text-gray-900">{{ $pizza->description }}</p>
                     </div>
-                    <p class="mt-4 text-lg text-gray-900">{{ $pizza->description }}</p>
                 </div>
-            </div>
             @endforeach
         </div>
-    </div>
+   </div>
 </x-app-layout>

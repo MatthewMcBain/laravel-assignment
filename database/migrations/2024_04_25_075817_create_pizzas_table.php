6cd68309b8516e80cf2784629f1624bc361d5898
Matthew McBain <matthewmcbain3@gmail.com>
2024-05-03 12:16:29 +0100

    comment out foreign topping id

--- database/migrations/2024_04_25_075817_create_pizzas_table.php
+++ database/migrations/2024_04_25_075817_create_pizzas_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('pizzas', function (Blueprint $table) {
             $table->increments('id');
-            $table->foreignId('topping_id')->constrained();
+            // $table->foreignId('topping_id')->constrained();
             $table->string('pizza_name');
             $table->string('description');
             $table->double('price')->default(0);

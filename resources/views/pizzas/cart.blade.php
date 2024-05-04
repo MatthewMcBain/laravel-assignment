ef934392c29a67a8868559386909759cf75809c5
Matthew McBain <matthewmcbain3@gmail.com>
2024-05-04 11:35:58 +0100

    change pizzas to cart

--- resources/views/pizzas/cart.blade.php
+++ resources/views/pizzas/cart.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Pizzas') }}
+            {{ __('Cart') }}
         </h2>
 
         <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">

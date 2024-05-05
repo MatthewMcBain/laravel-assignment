6726c0917a61a6bac952f4e30a68a41b6f6c0cd9
Matthew McBain <matthewmcbain3@gmail.com>
2024-05-05 11:29:58 +0100

    add to cart working tests

---
+++ tests/Feature/AddCartTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Pizza;
+
+class AddCartTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_add_pizza_to_cart(): void
+    {
+        // $user = User::factory()->create(['name' => 'Taylor']);
+        $this->seed();
+        $pizzas = Pizza::get();
+        // dd($pizzas[0]->pizza_name);
+        $cart = session('cart', collect([]));
+        $cart->push($pizzas[0]);
+        // $cart->push($pizzas[1]);
+        session(['cart' => $cart]);
+        // dd($cart);
+        $this->assertContains($pizzas[0], session('cart', collect([])));
+        $this->assertContains($pizzas[0], $cart);
+    }
+
+    public function test_add_pizzas_to_cart(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+
+        // $user = User::factory()->create(['name' => 'Taylor']);
+        $this->seed();
+        $pizzas = Pizza::get();
+        // dd($pizzas[0]->pizza_name);
+        $cart = session('cart', collect([]));
+        for ($i = 0; $i < 3; $i++) {
+            $cart->push($pizzas[$i]);
+        }
+        // $cart->push($pizzas[1]);
+        session(['cart' => $cart]);
+        // dd($cart);
+        for ($i = 0; $i < 3; $i++) {
+            $this->assertContains($pizzas[$i], session('cart', collect([])));
+            $this->assertContains($pizzas[$i], $cart);
+        }
+    }
+}
---
+++ tests/Feature/AddOrderTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Pizza;
+
+class AddOrderTest extends TestCase
+{
+    /**
+     * Testing the pizzas from the cart can be added to the order.
+     */
+    public function test_add_pizzas_from_cart_to_order(): void
+    {
+        // $user = User::factory()->create(['name' => 'Taylor']);
+        $this->seed();
+        $pizzas = Pizza::get();
+        // dd($pizzas[0]->pizza_name);
+        $cart = session('cart', collect([]));
+        for ($i = 0; $i < 3; $i++) {
+            $cart->push($pizzas[$i]);
+        }
+        // $cart->push($pizzas[1]);
+        session(['cart' => $cart]);
+        // dd($cart);
+        for ($i = 0; $i < 3; $i++) {
+            $this->assertContains($pizzas[$i], session('cart', collect([])));
+            $this->assertContains($pizzas[$i], $cart);
+        }
+
+        $order = $cart->user()->orders()->create();
+        $order->pizzas()->sync($cart->pizzas);
+        dd($order);
+        // $cart->session()->forget('cart');
+    }
+}

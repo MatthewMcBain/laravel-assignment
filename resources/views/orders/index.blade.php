1d57bc9da7a86c55dc4c2bcdc604162e7e04ab05
Matthew McBain <matthewmcbain3@gmail.com>
2024-05-05 17:09:38 +0100

    add total to orders pages

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -6,15 +6,18 @@
 
         <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
             @foreach ($orders as $order)
+            <div class="hidden">{{$runningTotal = 0}} </div>
             <div class="p-6 flex space-x-2">
                 <div class="flex-1">
                     <div class="flex justify-between items-center">
                         <div>
                             <span class="text-gray-800">{{ $order->id }}</span>
                             @foreach ($order->pizzas as $pizza)
-                                <p class="mt-4 text-lg text-gray-900">{{ $pizza->pizza_name }}</p>
-                                <p class="mt-4 text-lg text-gray-900">{{ $pizza->size }}</p>
+                            <div class="hidden">{{ $runningTotal += $pizza->price}} </div>
+                            <p class="mt-4 text-lg text-gray-900">{{ $pizza->pizza_name }}</p>
+                            <p class="mt-4 text-lg text-gray-900">{{ $pizza->size }}</p>
                             @endforeach
+                            <p class="mt-4 text-lg text-gray-900">Total: £{{ $runningTotal }} </p>
                         </div>
                         <a href="{{ route('orders.show', $order) }}" class="px-6 py-3 text-white no-underline bg-black rounded hover:bg-slate-800 hover:text-white">{{ __('View') }}</a>
                     </div>
--- resources/views/orders/order.blade.php
+++ resources/views/orders/order.blade.php
@@ -7,19 +7,26 @@
         <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
             <span class="text-gray-800">{{ $order->id }}</span>
             <form method="POST" action="{{ route('pizzas.order.add') }}">
+                <div class="hidden">{{$runningTotal = 0}} </div>
                 @csrf
                 @foreach ($order->pizzas as $pizza)
-                    <div class="p-6 flex space-x-2">
-                        <div class="flex-1">                                                     
-                            <p class="mt-4 text-lg text-gray-900">{{ $pizza->pizza_name }}</p>
-                            <p class="mt-4 text-lg text-gray-900">{{ $pizza->description }}</p>
-                            <p class="mt-4 text-lg text-gray-900">{{ $pizza->size }}</p>
-                            <p class="mt-4 text-lg text-gray-900">£{{ $pizza->price }}</p>
-                        </div>
+                <div class="hidden">{{ $runningTotal += $pizza->price}} </div>
+                <div class="p-6 flex space-x-2">
+                    <div class="flex-1">
+                        <p class="mt-4 text-lg text-gray-900">{{ $pizza->pizza_name }}</p>
+                        <p class="mt-4 text-lg text-gray-900">{{ $pizza->description }}</p>
+                        <p class="mt-4 text-lg text-gray-900">{{ $pizza->size }}</p>
+                        <p class="mt-4 text-lg text-gray-900">£{{ $pizza->price }}</p>
                     </div>
-                    <input type="hidden" name="pizzas[]" value="{{ $pizza->id }}" >                        
-                @endforeach                        
-                <x-primary-button class="mt-4">{{ __('Re-order') }}</x-primary-button>
+                </div>
+                <input type="hidden" name="pizzas[]" value="{{ $pizza->id }}">
+                @endforeach
+                <div class="pl-6 flex space-x-2">
+                    <div class="flex-1">
+                        <p class="mt-4 text-lg text-gray-900">Total: £{{ $runningTotal }} </p>
+                        <x-primary-button class="mt-4">{{ __('Re-order') }}</x-primary-button>
+                    </div>
+                </div>
             </form>
         </div>
     </div>

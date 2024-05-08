b66c49cc617b02a1adab631a483b7a2e39a07b16
Matthew McBain <matthewmcbain3@gmail.com>
2024-05-08 16:36:55 +0100

    cart tests working

--- tests/Feature/AddCartTest.php
+++ tests/Feature/AddCartTest.php
@@ -15,35 +15,26 @@ class AddCartTest extends TestCase
     
     public function test_add_pizza_to_cart(): void
     {
-        // $user = User::factory()->create(['name' => 'Taylor']);
         $this->seed();
         $pizzas = Pizza::get();
-        // dd($pizzas[0]->pizza_name);
         $cart = session('cart', collect([]));
         $cart->push($pizzas[0]);
-        // $cart->push($pizzas[1]);
         session(['cart' => $cart]);
-        // dd($cart);
         $this->assertContains($pizzas[0], $cart);
         $this->assertContains($pizzas[0], session('cart', collect([])));
         $this->assertNotContains($pizzas[1], $cart);
-        $this->assertNotContains($pizzas[1], session('cart', collect([])));
-        
+        $this->assertNotContains($pizzas[1], session('cart', collect([])));        
     }
 
     public function test_add_pizzas_to_cart(): void
     {
-        // $user = User::factory()->create(['name' => 'Taylor']);
         $this->seed();
         $pizzas = Pizza::get();
-        // dd($pizzas[0]->pizza_name);
         $cart = session('cart', collect([]));
         for ($i = 0; $i < 3; $i++) {
             $cart->push($pizzas[$i]);
         }
-        // $cart->push($pizzas[1]);
         session(['cart' => $cart]);
-        // dd($cart);
         for ($i = 0; $i < 3; $i++) {
             $this->assertContains($pizzas[$i], $cart);
             $this->assertContains($pizzas[$i], session('cart', collect([])));           
